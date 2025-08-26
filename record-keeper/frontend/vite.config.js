93cc427285dd8dcce78f1bd262d7cad638ade1d6
Supratik Deshmukh <89291911+supratikdeshmukh@users.noreply.github.com>
2025-08-26 23:15:51 +0530

    Update vite.config.js

--- record-keeper/frontend/vite.config.js
+++ record-keeper/frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // ✅ Vite configuration
 export default defineConfig({
+  base: "/projects/record-keeper/", // ✅ **Set your hosting base for folder/subfolder deployment not this**
   plugins: [react()], // ✅ Enable React + JSX support
 });
